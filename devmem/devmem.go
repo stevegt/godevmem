cc36e996071c64eea8ebe40a53092c07177b16cc
Steve Traugott <stevegt@t7a.org>
2022-07-02 12:26:24 -0700

    split most bits out into github.com/stevegt/godevmem/devmem library
    - clean up remaining C comments

---
+++ devmem/devmem.go
@@ -0,0 +1,97 @@
+package devmem
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"os"
+	"syscall"
+
+	"github.com/dolmen-go/endian"
+	. "github.com/stevegt/goadapt"
+	"golang.org/x/sys/unix"
+)
+
+var order = endian.Native
+
+type Mem struct {
+	page   []byte
+	target int64
+	size   int64
+	sizeb  int
+	base   int64
+	diff   int
+}
+
+func Open(target, size int64) (m *Mem, err error) {
+	defer Return(&err)
+	m = &Mem{}
+
+	fh, err := os.OpenFile("/dev/mem", os.O_RDWR|os.O_SYNC, os.ModePerm)
+	Ck(err)
+	defer fh.Close()
+	Pl("/dev/mem opened")
+
+	/* Map one page */
+	pageSize := os.Getpagesize()
+	m.base = target / int64(pageSize) * int64(pageSize)
+	m.diff = int(target - m.base)
+	Pf("target 0x%x pageSize 0x%x base 0x%x\n", target, pageSize, m.base)
+
+	prot := syscall.PROT_READ | syscall.PROT_WRITE
+	flags := syscall.MAP_SHARED
+	fd := int(fh.Fd())
+
+	m.page, err = unix.Mmap(fd, m.base, pageSize, prot, flags)
+	Ck(err)
+	Pl("memory mapped")
+
+	if !(size == 8 || size == 16 || size == 32 || size == 64) {
+		err = fmt.Errorf("invalid bit width: %d\n", size)
+		return
+	}
+
+	m.size = size
+	m.sizeb = int(size / 8)
+	return
+}
+
+func (m *Mem) Read() {
+	raw := m.page[m.diff : m.diff+m.sizeb]
+	Pf("raw 0x%x\n", raw)
+	switch len(raw) * 8 {
+	case 8:
+		cooked := uint8(raw[0])
+		Pf("read 0x%x\n", cooked)
+	case 16:
+		cooked := order.Uint16(raw)
+		Pf("read 0x%x\n", cooked)
+	case 32:
+		cooked := order.Uint32(raw)
+		Pf("read 0x%x\n", cooked)
+	case 64:
+		cooked := order.Uint64(raw)
+		Pf("read 0x%x\n", cooked)
+	default:
+	}
+}
+
+func (m *Mem) Write(newVal uint64) (err error) {
+	defer Return(&err)
+	wr := bytes.NewBuffer(make([]byte, 0, m.sizeb))
+	err = binary.Write(wr, order, newVal)
+	Ck(err)
+	newRaw := wr.Bytes()
+	for i := 0; i < m.sizeb; i++ {
+		m.page[m.diff+i] = newRaw[i]
+	}
+	Pf("write 0x%x\n", newVal)
+	return
+}
+
+func (m *Mem) Close() (err error) {
+	defer Return(&err)
+	err = unix.Munmap(m.page)
+	Ck(err)
+	return
+}
--- main.go
+++ main.go
@@ -1,15 +1,11 @@
 package main
 
 import (
-	"bytes"
-	"encoding/binary"
 	"os"
 	"strconv"
-	"syscall"
 
-	"github.com/dolmen-go/endian"
 	. "github.com/stevegt/goadapt"
-	"golang.org/x/sys/unix"
+	"github.com/stevegt/godevmem/devmem"
 )
 
 /*
@@ -59,104 +55,12 @@ import (
  *
  */
 
-/*
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdint.h>
-#include <unistd.h>
-#include <string.h>
-#include <errno.h>
-#include <signal.h>
-#include <fcntl.h>
-#include <ctype.h>
-#include <termios.h>
-#include <sys/types.h>
-#include <sys/mman.h>
-
-#define FATAL do { fprintf(stderr, "Error at line %d, file %s (%d) [%s]\n", \
-  __LINE__, __FILE__, errno, strerror(errno)); exit(1); } while(0)
-
-
-static inline void *fixup_addr(void *addr, size_t size);
-*/
-
-var order = endian.Native
-
 var usage = `
 Usage: %s { address } [ size [ data ] ]
 	address : memory address to act upon
 	size    : data bit width (default 32): 8, 16, 32, 64
 	data    : data to be written
 `
-
-type Mem struct {
-	page   []byte
-	target int64
-	size   int64
-	sizeb  int
-	base   int64
-	diff   int
-}
-
-func MMap(fh *os.File, target, size int64) (m *Mem) {
-	m = &Mem{}
-	/* Map one page */
-	pageSize := os.Getpagesize()
-	m.base = target / int64(pageSize) * int64(pageSize)
-	m.diff = int(target - m.base)
-	Pf("target 0x%x pageSize 0x%x base 0x%x\n", target, pageSize, m.base)
-
-	prot := syscall.PROT_READ | syscall.PROT_WRITE
-	flags := syscall.MAP_SHARED
-	fd := int(fh.Fd())
-
-	var err error
-	m.page, err = unix.Mmap(fd, m.base, pageSize, prot, flags)
-	Ck(err)
-	Pl("memory mapped")
-
-	m.sizeb = int(size / 8)
-	return
-}
-
-func (m *Mem) read() {
-	raw := m.page[m.diff : m.diff+m.sizeb]
-	Pf("raw 0x%x\n", raw)
-	switch len(raw) * 8 {
-	case 8:
-		cooked := uint8(raw[0])
-		// rd := bytes.NewReader(raw)
-		// err := binary.Read(rd, order, &cooked)
-		// Ck(err)
-		Pf("read 0x%x\n", cooked)
-	case 16:
-		cooked := order.Uint16(raw)
-		Pf("read 0x%x\n", cooked)
-	case 32:
-		cooked := order.Uint32(raw)
-		Pf("read 0x%x\n", cooked)
-	case 64:
-		cooked := order.Uint64(raw)
-		Pf("read 0x%x\n", cooked)
-	default:
-	}
-}
-
-func (m *Mem) write(newVal uint64) {
-	wr := bytes.NewBuffer(make([]byte, 0, m.sizeb))
-	err := binary.Write(wr, order, newVal)
-	Ck(err)
-	newRaw := wr.Bytes()
-	for i := 0; i < m.sizeb; i++ {
-		m.page[m.diff+i] = newRaw[i]
-	}
-	Pf("write 0x%x\n", newVal)
-}
-
-func (m *Mem) Unmap() {
-	err := unix.Munmap(m.page)
-	Ck(err)
-}
 
 func main() {
 
@@ -172,10 +76,6 @@ func main() {
 	if len(os.Args) > 2 {
 		size, err = strconv.ParseInt(os.Args[2], 0, 64)
 		Ck(err)
-		if !(size == 8 || size == 16 || size == 32 || size == 64) {
-			Fpf(os.Stderr, "invalid bit width: %d\n", size)
-			os.Exit(1)
-		}
 	}
 
 	var newVal uint64
@@ -186,19 +86,15 @@ func main() {
 		write = true
 	}
 
-	fh, err := os.OpenFile("/dev/mem", os.O_RDWR|os.O_SYNC, os.ModePerm)
+	mem, err := devmem.Open(target, size)
 	Ck(err)
-	defer fh.Close()
-	Pl("/dev/mem opened")
 
-	mem := MMap(fh, target, size)
-
-	mem.read()
+	mem.Read()
 	if write {
-		mem.write(newVal)
-		mem.read()
+		mem.Write(newVal)
+		mem.Read()
 	}
 
-	mem.Unmap()
+	mem.Close()
 
 }
